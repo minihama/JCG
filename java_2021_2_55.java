f0a96db1da101325b5f4388a27f35e38b76a0447
minihama <minihama@hotmail.co.kr>
2022-04-06 10:27:16 +0900

    add java_2021_2_55

---
+++ java_2021_2_55.java
@@ -0,0 +1,11 @@
+public class java_2021_2_55 {
+
+    public static void main(String []args){
+        int a = 20;
+        int b = 30;
+
+        System.out.println(++a);
+        System.out.println(b--);
+
+    }
+}
